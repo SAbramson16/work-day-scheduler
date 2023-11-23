5d7818a9d95f377dd489dabfd4cbc01b47c8fbfc
Sharon Abramson <sharabramson@gmail.com>
2023-11-23 17:14:06 -0500

    created changeColor function, created loadItems function, created click function on save button, adjusted ID's in html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+let savedItem = []
+let timeBlock = document.querySelectorAll(".time-block");
+let dayDisplayEl = $('#currentDay');
+let currentHour = dayjs().hour();
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  function loadItems() {
+    let savedItem = JSON.parse(localStorage.getItem("savedItem"));
+    savedItem.forEach(function (task) {
+      $('#' + task.hour + ' .description').val(task.text)
+    })
+  }
+  
+  loadItems();
+  displayDay();
+  
+  function displayDay() {
+    let currentDay = dayjs().format('dddd MMM DD, YYYY hh:mm a');
+    // console.log(currentDay);
+    dayDisplayEl.text(currentDay);
+  }
+
+    function changeColor() {
+      $('.time-block').each(function() {
+        let timeBlock = parseInt(this.id);
+        if (currentHour > timeBlock) {
+          $(this).addClass('past');
+        } if (currentHour === timeBlock) {
+          $(this).addClass('present');
+        } if (currentHour < timeBlock) {
+          $(this).addClass('future');
+        }
+      })
+    }
+
+    changeColor();
+    console.log("change color");
+  
+  $('.saveBtn').on('click', function() {
+    let saveBtn = $(this);
+    let textInput = saveBtn.siblings('.description').val();
+    let time = saveBtn.closest('.time-block').attr('id');
+    // console.log(textInput);
+    // console.log(time);
+
+    task = {
+      text: textInput,
+      hour: time,
+    }
+    console.log(task);
+    let savedItem = JSON.parse(localStorage.getItem("savedItem")) || [];
+    savedItem.push(task);
+    localStorage.setItem("savedItem", JSON.stringify(savedItem));
+  })
+
 });
